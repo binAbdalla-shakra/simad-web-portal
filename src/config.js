8f4f51f78b64078a8f9a6d1ef280a2f2bb12ff5d
Abdishakur <yareshakra6@gmail.com>
2025-10-02 19:05:41 +0300

    update

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ module.exports = {
     APP_ID: "",
   },
   api: {
-    API_URL: "http://localhost:4000/api/v1/admin",
-    // API_URL: "https://simad-portal-api.vercel.app/api/v1",
+    // API_URL: "http://localhost:4000/api/v1/admin",
+    API_URL: "https://simad-portal-api.vercel.app/api/v1",
 
   }
 };
